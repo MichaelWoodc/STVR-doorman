9ee3041415fcb592eeaddaf6d65c5d2882824085
michaelwoodc <michaelwoodcock@hotmail.com>
2026-07-22 15:22:35 -0400

    add new properties and complaints

--- data/locations.js
+++ data/locations.js
@@ -9,5 +9,16 @@ const stvrLocations = [
     "airbnbUrl": "",
     "otherUrl": "",
     "viewComplaintsUrl": "data/complaints/8611_whitefield_ave_savannah_ga_31406/main_unit/index.html"
+  },
+  {
+    "title": "Licensed STVR Property",
+    "address": "9240 GARLAND DR, SAVANNAH, GA 31406",
+    "license": "",
+    "lat": 31.9803637,
+    "lng": -81.1069629,
+    "vrboUrl": "https://www.vrbo.com/4865092",
+    "airbnbUrl": "https://www.airbnb.com/rooms/1501030060746906896",
+    "otherUrl": "https://michaelwoodc.github.io/stvr-info-page/owners/cozy_corner_rentals/cozy_corner_rentals.html",
+    "viewComplaintsUrl": "data/complaints/9240_garland_dr_savannah_ga_31406/none/index.html"
   }
 ];
