195506845aa8f79f88859f983d9a9efe7587852b
michaelwoodc <michaelwoodcock@hotmail.com>
2026-07-22 13:28:18 -0400

    finalized creation code to fix urls/paths

--- data/locations.js
+++ data/locations.js
@@ -5,18 +5,9 @@ const stvrLocations = [
     "license": "",
     "lat": 31.9874255,
     "lng": -81.1036068,
-    "vrboUrl": "https://www.vrbo.com/3799837",
-    "airbnbUrl": "",
-    "viewComplaintsUrl": "data/complaints/8611_whitefield_ave_savannah_ga_31406/svr-12345_placeholder/index.html"
-  },
-  {
-    "title": "Licensed STVR Property",
-    "address": "9240 GARLAND DR, SAVANNAH, GA 31406",
-    "license": "",
-    "lat": 31.9803637,
-    "lng": -81.1069629,
     "vrboUrl": "",
     "airbnbUrl": "",
-    "viewComplaintsUrl": "data/complaints/9240_garland_dr_savannah_ga_31406/none/index.html"
+    "otherUrl": "",
+    "viewComplaintsUrl": "data/complaints/8611_whitefield_ave_savannah_ga_31406/main_unit/index.html"
   }
 ];
